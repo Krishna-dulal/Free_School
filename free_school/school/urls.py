13fe98aacb3d9d49382743bb28b8dd05d0bc4bec
Krishna Dulal <npu9jc@virginia.edu>
2024-01-28 23:53:08 -0500

    make some progess with ref of schedular projectt

--- free_school/school/urls.py
+++ free_school/school/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
-app_name = 'school'  # Replace 'myapp' with the actual app name
+app_name = 'school'  
 
 urlpatterns = [
     path("", views.home, name="home"),
+     path('<int:id>/', views.school, name='school'),
     path("logout/", views.logout_view, name="logout"),
 ]
--- free_school/school/views.py
+++ free_school/school/views.py
@@ -3,10 +3,31 @@ from django.contrib.auth import logout
 # Create your views here.
 
 
+
 def home(request):
-    return render(request, "home.html")
+    user = request.user
+        #print(user)
+    if (userIsInGroup(user, 'Student')):
+        return studentIndex(request)
+    elif (userIsInGroup(user, 'Teacher')):
+        return advisorIndex(request)
+    else:
+        return render(request, 'home.html')
 
 
 def logout_view(request):
     logout(request)
     return redirect("/")
+
+def userIsInGroup(user, groupName):
+    return user.groups.filter(name=groupName).exists()
+
+def school(request, id):
+    # Load the appropriate template based on user's group
+    if userIsInGroup(request.user, 'Student'):
+        template = loader.get_template('student.html')
+    elif userIsInGroup(request.user, 'Teacher'):
+        template = loader.get_template('teacher.html')
+    else:
+        # Redirect or show an error if the user is neither a student nor a teacher
+        return redirect('/')
