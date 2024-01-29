cf37d8685edbab6dcc6b8e277373e3c3a1e2b322
Krishna Dulal <npu9jc@virginia.edu>
2024-01-29 15:55:30 -0500

    Some dropdown link added to navbar.

--- free_school/school/views.py
+++ free_school/school/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 # Create your views here.
+from .models import UserProfile
+
+def your_google_login_callback_view(request):
+    user = request.user
+    UserProfile.objects.get_or_create(user=user)
+    # rest of your view logic
+
 
 
 
 def home(request):
     user = request.user
-        #print(user)
-    if (userIsInGroup(user, 'Student')):
+    context = {}  # Initialize an empty context dictionary
+
+    if userIsInGroup(user, 'Student'):
+        context['role'] = 'Student'
         return studentIndex(request)
-    elif (userIsInGroup(user, 'Teacher')):
+    elif userIsInGroup(user, 'Teacher'):
+        context['role'] = 'Teacher'
         return advisorIndex(request)
     else:
-        return render(request, 'home.html')
+        return render(request, 'home.html', context)
 
 
 def logout_view(request):
